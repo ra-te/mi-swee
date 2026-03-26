ac3a08ac1eb830650fbca8cb13cd661ebcf556c7
Raphael Teller <raphael.teller@stud-mail.uni-wuerzburg.de>
2026-03-26 21:46:28 +0100

    feat: more essential functionality for minefield class

--- src/minefield.py
+++ src/minefield.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from colorama import init as init_colorama
 from colorama import Back
 
 
@@ -19,9 +20,33 @@ class Minefield():
 
         self.width = width
         self.height = height
-        self.board = np.zeros(shape=(width, height), dtype=np.uint8)
-        self.mines = np.zeros(shape=(width, height), dtype=np.uint8)
+        self.flags = np.zeros(shape=(width, height), dtype=np.bool)
+        self.opens = np.zeros(shape=(width, height), dtype=np.bool)
+        self.mines = np.zeros(shape=(width, height), dtype=np.bool)
         self.infos = np.zeros(shape=(width, height), dtype=np.uint8)
+
+
+    def set_num_mines(self, flat: bool, val: int=None, mean: float=None, var: float=None):
+        """
+        Sets the number of mines for the field.
+
+        Args:
+            flat (bool): Whether a flat value is selected or sampling from a normal distribution.
+            val (int, optional): If `flat` is enabled, this value is set to be the number of mines. 
+            mean (float, optional): If `flat` is disabled, this value represents the mean of the normal distribution to sample from.
+            var (float, optional): If `flat` is disabled, this value represents the variance of the normal distribution to sample from.
+        """
+
+        if flat:
+            if not val or self.width * self.height < val or val < 0:
+                raise ValueError(f"Invalid number of mines, must be greater than 0 and smaller than {self.width * self.height + 1}.")
+            else:
+                self.num_mines = val
+        else:
+            if not mean or not var:
+                raise ValueError("If random sampling is enabled, mean and variance must be passed as values.")
+            else:
+                self.num_mines = int(np.clip(np.random.normal(loc=mean, scale=var), 0, self.width * self.height))
 
 
     def get_neighbors(self, pos: tuple[int, int]) -> list:
@@ -65,24 +90,6 @@ class Minefield():
 
         return mines
 
-
-    def set_mines(self, mine_grid: np.ndarray):
-        """
-        Place the mines on to the field.
-
-        Args:
-            mine_grid (np.ndarray): An array of booleans representing the positions of the mines to place.
-        """
-
-        if mine_grid.shape != self.board.shape:
-            raise ValueError(f"Width and height do not match the minefield. Expected: ({self.board.shape}) but received: {mine_grid.shape}.")
-        
-        if not np.isin(mine_grid, [0, 1]).all():
-            raise ValueError(f"Given grid contains invalid values: {np.unique(mine_grid)}.")
-        
-        self.mines = mine_grid.astype(dtype=np.uint8)
-        self.calc_infos()
-
     
     def calc_infos(self):
         """
@@ -93,60 +100,138 @@ class Minefield():
             for c in range(self.height):
                 self.infos[r][c] = self.check_mines(self.get_neighbors((r, c))).count(True)
 
-
-    def set_num_mines(self, flat: bool, val: int=None, mean: float=None, var: float=None):
-        """
-        Sets the number of mines for the field.
-
-        Args:
-            flat (bool): Whether a flat value is selected or sampling from a normal distribution.
-            val (int, optional): If `flat` is enabled, this value is set to be the number of mines. 
-            mean (float, optional): If `flat` is disabled, this value represents the mean of the normal distribution to sample from.
-            var (float, optional): If `flat` is disabled, this value represents the variance of the normal distribution to sample from.
-        """
-
-        if flat:
-            if not val or self.width * self.height < val or val < 0:
-                raise ValueError(f"Invalid number of mines, must be greater than 0 and smaller than {self.width * self.height + 1}.")
-            else:
-                self.num_mines = val
-        else:
-            if not mean and not var:
-                raise ValueError("If random sampling is enabled, mean and variance must be passed as values.")
-            else:
-                self.num_mines = int(np.clip(np.random.normal(loc=mean, scale=var), 0, self.width * self.height))
-
-
+    
     def sample_mines(self):
         """
-        Places mines randomly on to the field.
+        Places mines randomly on to the field and calculates board information.
         """
 
         grid = np.zeros(shape=(self.width * self.height))
         grid[:self.num_mines] = 1
         np.random.shuffle(grid)
-        self.set_mines(grid.reshape((self.width, self.height)))
+        self.mines = grid.reshape((self.width, self.height)).astype(dtype=np.bool)
+        self.calc_infos()
 
 
-    def print_field(self, show_mines: bool=False):
+    def open_cell(self, pos: tuple[int, int]) -> bool:
+        """
+        Opens the cell at the given position. Returns None if cell is already open, True if cell was safe and False if not.
+        If cell was safe and has 0 neighboring mines, automatically opens all cells in proximity that also have 0 neighboring mines.
+
+        Args:
+            pos (tuple[int, int]): The position of the cell to open.
+        """
+
+        r, c = pos
+        if self.opens[r][c]:
+            return None
+
+        # Reveal the clicked cell
+        self.opens[r][c] = True
+        self.flags[r][c] = False
+
+        # If mine at that position, return False
+        if self.mines[r][c]:
+            return False
+
+        # Stop if cell has at least one neighboring mine
+        if self.infos[r][c] != 0:
+            return True
+
+        # Flood fill logic
+        candidates = list()
+        candidates.append(pos)
+        visited = set()
+        visited.add(pos)
+
+        while candidates:
+            curr_pos = candidates.pop(0)
+
+            # Get all neighbors
+            for neighbor_pos in self.get_neighbors(curr_pos):
+                n_r, n_c = neighbor_pos
+                if neighbor_pos in visited:
+                    continue
+                
+                # Open the cell of the neighbor
+                self.opens[n_r][n_c] = True
+                self.flags[n_r][n_c] = False
+                visited.add(neighbor_pos)
+
+                # If the neighbor has 0 neighboring mines, add to candidates
+                if self.infos[n_r][n_c] == 0:
+                    candidates.append(neighbor_pos)
+
+        return True
+            
+    
+    def set_flag(self, pos: tuple[int, int]) -> bool:
+        """
+        Toggles a flag at the given position. Returns None if cell is already open, True if flag was set and False if flag was removed.
+
+        Args:
+            pos (tuple[int, int]): The position of the cell to toggle flag.
+        """
+
+        r, c = pos
+        if self.opens[r][c]:
+            return None
+        else:
+            self.flags[r][c] = not self.flags[r][c]
+            return self.flags[r][c]
+
+
+    def print_field(self, show_truth: bool=False):
         """
         Prints the field.
 
         Args:
-            show_mines (bool): Whether mines should be printed or not.
+            show_truth (bool): Whether mines should be printed or not, and if current flags are correct or not.
         """
 
-        raise NotImplementedError()
-
+        init_colorama()
         for r in range(self.width):
             for c in range(self.height):
-                cell = "X"
-                print(cell, end=" ")
-            print()
+                text = " "
+                color = Back.RESET
+
+                # Text selection
+                if self.flags[r][c]:
+                    text = "F"
+                else:
+                    if self.mines[r][c]:
+                        if self.opens[r][c]:
+                            text = "D"
+                        else:
+                            if show_truth:
+                                text = "M"
+                    else:
+                        if self.opens[r][c]:
+                            val = self.infos[r][c]
+                            if val != 0:
+                                text = str(val)
+
+                # Background color selection
+                if not self.opens[r][c]:
+                    if show_truth:
+                        if self.flags[r][c] and not self.mines[r][c]:
+                            color = Back.RED
+                        else:
+                            color = Back.GREEN
+                    else:
+                        color = Back.WHITE
+
+                print(color + text, end=" ")
+            
+            print(Back.RESET)
         
 
 if __name__ == "__main__":
-    field = Minefield(3, 3)
-    field.set_num_mines(flat=False, val=1, mean=5, var=3)
+    field = Minefield(10, 10)
+    field.set_num_mines(flat=False, val=2, mean=6, var=2)
     field.sample_mines()
-    field.print_field()
+    print(field.num_mines)
+    field.print_field(True)
+    print("")
+    print(field.open_cell((2, 2)))
+    field.print_field(True)
